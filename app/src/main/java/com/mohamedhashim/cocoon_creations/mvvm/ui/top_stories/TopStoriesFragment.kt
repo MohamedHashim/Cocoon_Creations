45c4b728af0042ecdac03479443069dc3b09d93f
MohamedHashim <sawmido2000@gmail.com>
2020-11-19 19:24:28 +0200

    Implement the fav button to bookmark a story

--- app/src/main/java/com/mohamedhashim/cocoon_creations/mvvm/ui/top_stories/TopStoriesFragment.kt
+++ app/src/main/java/com/mohamedhashim/cocoon_creations/mvvm/ui/top_stories/TopStoriesFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.mohamedhashim.cocoon_creations.R
 import com.mohamedhashim.cocoon_creations.common_ui.adapters.TopStoriesAdapter
 import com.mohamedhashim.cocoon_creations.common_ui.extensions.showTopStories
@@ -13,6 +14,7 @@ import com.mohamedhashim.cocoon_creations.mvvm.base.DataBindingFragment
 import com.skydoves.baserecyclerviewadapter.RecyclerViewPaginator
 import kotlinx.android.synthetic.main.fragment_top_stories.*
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 /**
  * Created by Mohamed Hashim on 11/18/2020.
